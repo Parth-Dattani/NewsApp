ed9eecb2341a3aaebf0a1ae66eba236b62340b7f
parth <dattaniparth2@gmail.com>
2023-04-03 12:17:04 +0530

    chage App Icon

--- lib/screen/News/news_screen.dart
+++ lib/screen/News/news_screen.dart
@@ -14,74 +14,74 @@ class NewsScreen extends GetView<NewsController> {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      ()=> CommonLoader( isLoad: controller.loader.value && controller.resultDataList.isEmpty,
-      body: /*controller.resultDataList.isEmpty ? CircularProgressIndicator() :*/ Scaffold(
-        backgroundColor: ColorsConfig.colorWhite,
-        appBar: CommonAppBar(
-          leadingIcon: ImagePath.appLogo,
-          leadingOnTap: () {},
-          leadingWidth: 150,
-          iconSize: 4.5,
-          actionIcon: ImagePath.notificationIcon ,
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding:
-            const EdgeInsets.only(left: 25, right: 25, top: 40, bottom: 16),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "latest".tr,
-                      style: CustomTextStyle.appBarText,
-                    ),
-                    TextButton(
-                      child: Text(
-                        "see_all".tr,
-                        style: CustomTextStyle.labelStyle,
+          ()=> CommonLoader( isLoad: controller.loader.value && controller.resultDataList.isEmpty,
+        body: /*controller.resultDataList.isEmpty ? CircularProgressIndicator() :*/ Scaffold(
+          backgroundColor: ColorsConfig.colorWhite,
+          appBar: CommonAppBar(
+            leadingIcon: ImagePath.appLogo,
+            leadingOnTap: () {},
+            leadingWidth: 150,
+            iconSize: 4.5,
+            actionIcon: ImagePath.notificationIcon ,
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding:
+              const EdgeInsets.only(left: 25, right: 25, top: 40, bottom: 16),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "latest".tr,
+                        style: CustomTextStyle.appBarText,
                       ),
-                      onPressed: () {
-                        //Get.toNamed(NewsScreen.pageId);
-                      },
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                // DefaultTabController(
-                //   length: controller.tabList.length,
-                //   child: TabBar(
-                //       isScrollable: true,
-                //       labelPadding: const EdgeInsets.only(right: 10),
-                //       controller: controller.tabController,
-                //       labelColor: ColorsConfig.colorWhite,
-                //       indicatorWeight: 5,
-                //       indicatorSize: TabBarIndicatorSize.label,
-                //       unselectedLabelColor: ColorsConfig.colorBlack,
-                //       onTap: (e){
-                //         controller.category = controller.tabList[e];
-                //         controller.categoryNews(controller.category);
-                //       },
-                //       tabs: List.generate(
-                //           controller.tabList.length,
-                //               (index) => Tab(
-                //
-                //             child: Text(
-                //               controller.tabList[index],
-                //               style: CustomTextStyle.newsHeadLineText,
-                //             ),
-                //           ))
-                //
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                Flexible(
-                  child:  TabBarView(
+                      TextButton(
+                        child: Text(
+                          "see_all".tr,
+                          style: CustomTextStyle.labelStyle,
+                        ),
+                        onPressed: () {
+                          //Get.toNamed(NewsScreen.pageId);
+                        },
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  DefaultTabController(
+                    length: controller.tabList.length,
+                    child: TabBar(
+                        isScrollable: true,
+                        labelPadding: const EdgeInsets.only(right: 10),
+                        controller: controller.tabController,
+                        labelColor: ColorsConfig.colorWhite,
+                        indicatorWeight: 5,
+                        indicatorSize: TabBarIndicatorSize.label,
+                        unselectedLabelColor: ColorsConfig.colorBlack,
+                        onTap: (e){
+                          controller.category = controller.tabList[e];
+                          controller.categoryNews(controller.category);
+                        },
+                        tabs: List.generate(
+                            controller.tabList.length,
+                                (index) => Tab(
+
+                              child: Text(
+                                controller.tabList[index],
+                                style: CustomTextStyle.newsHeadLineText,
+                              ),
+                            ))
+
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Flexible(
+                    child:  TabBarView(
                         physics: const NeverScrollableScrollPhysics(),
                         controller: controller.tabController,
                         children:
@@ -89,7 +89,7 @@ class NewsScreen extends GetView<NewsController> {
                             controller.tabList.length,
                                 (index) {
                               return Obx(
-                                ()=> ListView.separated(
+                                    ()=> ListView.separated(
                                     itemCount: controller.resultDataList.length,
                                     itemBuilder: (context, index) {
                                       return GestureDetector(
@@ -139,13 +139,167 @@ class NewsScreen extends GetView<NewsController> {
                       //   Tab(text: "s"),
                       // ],
                     ),
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-      ),
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:news_app/screen/Home/widget/news_list_widget.dart';
+// import '../../constant/constant.dart';
+// import '../../controller/controller.dart';
+// import '../../widgets/widgets.dart';
+// import '../screen.dart';
+//
+// class NewsScreen extends GetView<NewsController> {
+//   static const pageId = "/NewsScreen";
+//
+//   const NewsScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Obx(
+//       ()=> CommonLoader( isLoad: controller.loader.value && controller.resultDataList.isEmpty,
+//       body: /*controller.resultDataList.isEmpty ? CircularProgressIndicator() :*/ Scaffold(
+//         backgroundColor: ColorsConfig.colorWhite,
+//         appBar: CommonAppBar(
+//           leadingIcon: ImagePath.appLogo,
+//           leadingOnTap: () {},
+//           leadingWidth: 150,
+//           iconSize: 4.5,
+//           actionIcon: ImagePath.notificationIcon ,
+//         ),
+//         body: SafeArea(
+//           child: Padding(
+//             padding:
+//             const EdgeInsets.only(left: 25, right: 25, top: 40, bottom: 16),
+//             child: Column(
+//               children: [
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text(
+//                       "latest".tr,
+//                       style: CustomTextStyle.appBarText,
+//                     ),
+//                     TextButton(
+//                       child: Text(
+//                         "see_all".tr,
+//                         style: CustomTextStyle.labelStyle,
+//                       ),
+//                       onPressed: () {
+//                         //Get.toNamed(NewsScreen.pageId);
+//                       },
+//                     )
+//                   ],
+//                 ),
+//                 const SizedBox(
+//                   height: 15,
+//                 ),
+//                 // DefaultTabController(
+//                 //   length: controller.tabList.length,
+//                 //   child: TabBar(
+//                 //       isScrollable: true,
+//                 //       labelPadding: const EdgeInsets.only(right: 10),
+//                 //       controller: controller.tabController,
+//                 //       labelColor: ColorsConfig.colorWhite,
+//                 //       indicatorWeight: 5,
+//                 //       indicatorSize: TabBarIndicatorSize.label,
+//                 //       unselectedLabelColor: ColorsConfig.colorBlack,
+//                 //       onTap: (e){
+//                 //         controller.category = controller.tabList[e];
+//                 //         controller.categoryNews(controller.category);
+//                 //       },
+//                 //       tabs: List.generate(
+//                 //           controller.tabList.length,
+//                 //               (index) => Tab(
+//                 //
+//                 //             child: Text(
+//                 //               controller.tabList[index],
+//                 //               style: CustomTextStyle.newsHeadLineText,
+//                 //             ),
+//                 //           ))
+//                 //
+//                 //   ),
+//                 // ),
+//                 // const SizedBox(
+//                 //   height: 20,
+//                 // ),
+//                 Flexible(
+//                   child:  TabBarView(
+//                         physics: const NeverScrollableScrollPhysics(),
+//                         controller: controller.tabController,
+//                         children:
+//                         List.generate(
+//                             controller.tabList.length,
+//                                 (index) {
+//                               return Obx(
+//                                 ()=> ListView.separated(
+//                                     itemCount: controller.resultDataList.length,
+//                                     itemBuilder: (context, index) {
+//                                       return GestureDetector(
+//                                         onTap: (){
+//                                           Get.toNamed(DetailScreen.pageId,
+//                                               arguments: {
+//                                                 'section': controller.resultDataList[index].section.toString(),
+//                                                 'title': controller.resultDataList[index].title.toString(),
+//                                                 'byLine' : controller.resultDataList[index].orgFacet != null
+//                                                     ? controller.resultDataList[index].orgFacet![0].toString()
+//                                                     : '',
+//                                                 'publishedDate': controller.resultDataList[index].publishedDate.toString(),
+//                                                 'image' :controller.resultDataList[index].multimedia != null ?
+//                                                 controller.resultDataList[index].multimedia![0].url
+//                                                     .toString() : '',
+//                                                 'abstract' :controller.resultDataList[index].abstract.toString()
+//                                               });
+//
+//                                         },
+//                                         child: NewsListWidget(
+//                                           section: controller.resultDataList[index].section.toString(),
+//                                           title: controller.resultDataList[index].title.toString(),
+//                                           byLine: controller.resultDataList[index].orgFacet!.isNotEmpty
+//                                               ? controller
+//                                               .resultDataList[index].orgFacet!.first
+//                                               .toString()
+//                                               : '',
+//                                           newsLink : controller.resultDataList[index].url.toString(),
+//                                           publishedDate:
+//                                           controller.resultDataList[index].publishedDate.toString(),
+//                                           image: controller.resultDataList[index].multimedia != null
+//                                               ? controller
+//                                               .resultDataList[index].multimedia![0].url
+//                                               .toString()
+//                                               : '',
+//                                         ),
+//                                       );
+//                                     },
+//                                     separatorBuilder: (BuildContext context, int index) {
+//                                       return Container(height: 35.0);
+//                                     }),
+//                               );
+//
+//                             })
+//                       // const [
+//                       //   NewsListWidget(itemCount: 20),
+//                       //   Tab(text: "s"),
+//                       // ],
+//                     ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//       ),
+//     );
+//   }
+// }
